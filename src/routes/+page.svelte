53e8a50b23098d240e822ae93b1a5ef99d924aec
sideagle <lakhani.siddhant@gmail.com>
2023-02-20 21:44:07 -0600

    added sidebar with socials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,23 @@
-Can't wait to make an awesome site!
+<script>
+</script>
+
+<!-- page wrapper -->
+<section class="flex sm:flex-row flex-col h-[100vh]">
+	<section
+		class="sm:h-full h-auto w-full sm:w-auto p-5 sm:border-r border-b border-stone-200 flex sm:flex-col justify-between"
+	>
+		<img src="/logo.png" alt="logo" class="w-20 h-auto" />
+		<div class="sm:h-36 sm:w-full w-52 items-center flex sm:flex-col justify-between">
+			<a href="#"><i class="fa-brands fa-instagram" /></a>
+			<span class="font-bold"> · </span>
+			<a href="#"><i class="fa-brands fa-twitter" /></a>
+			<span class="font-bold"> · </span>
+			<a href="#"><i class="fa-brands fa-youtube" /></a>
+		</div>
+
+		<!-- social media links (font awesome icons) -->
+
+		<!-- empty div for alignment purposes -->
+		<div />
+	</section>
+</section>
